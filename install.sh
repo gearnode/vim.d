c22a6c43fb162f8fd2b3b3a7667d3f1a12eb08f0
Gearnode <bfrimin@student.42.fr>
2016-08-28 22:56:54 +0200

    Tested install script

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
+#!/bin/bash
+
 DOWNLOAD_URL=https://github.com/gearnode/vim.d.git
-INSTALL_PATH=~dotfiles/vimconfig
+INSTALL_PATH=$HOME/vimconfig
 
 mkdir -p $INSTALL_PATH
 git clone $DOWNLOAD_URL $INSTALL_PATH
-mv ~/.vim ~/.vim_copy
-ln -s $INSTALL_PATH ~/.vim
+mv $HOME/.vim $HOME/.vim_copy
+mv $INSTALL_PATH $HOME/.vim
 
-echo -n "Lauch vim and type :PlugInstall"
-echo -n "Do you have any errors? (Y/n)"
-
-response=y
-read response
-if [ $(response) -eq "y" ]; then
-  rm -rf ~/.vim_copy
-  echo -n "Your vim is now configurate !"
+echo "Lauch vim and type :PlugInstall"
+read -p "Do you have any errors? (Y/n):" response
+if [ "$response" == "y" ] || [[ -z "${response// }" ]]; then
+  rm -rf $HOME/.vim_copy
+  echo "Your vim configuration is updated !"
 else
-  rm -rf ~/.vim
-  mv ~/.vim_copy ~/.vim
-  echo -n "Something was wrong your config is rollback"
+  rm -rf $HOME/.vim
+  mv $HOME/.vim_copy $HOME/.vim
+  echo "Something was wrong..."
+  echo "Your configuration is rollback"
 fi
