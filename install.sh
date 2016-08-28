dd0b55c3788956fa3f71d5213de62172bf7049db
Gearnode <bfrimin@student.42.fr>
2016-08-28 22:22:16 +0200

    Add smart install

--- install.sh
+++ install.sh
@@ -1,2 +1,21 @@
-mv ~/.vim ~/.vim_old
-git clone https://github.com/gearnode/vim.d.git ~/.vim
+DOWNLOAD_URL=https://github.com/gearnode/vim.d.git
+INSTALL_PATH=~dotfiles/vimconfig
+
+mkdir -p $INSTALL_PATH
+git clone $DOWNLOAD_URL $INSTALL_PATH
+mv ~/.vim ~/.vim_copy
+ln -s $INSTALL_PATH ~/.vim
+
+echo -n "Lauch vim and type :PlugInstall"
+echo -n "Do you have any errors? (Y/n)"
+
+response=y
+read response
+if [ $(response) -eq "y" ]; then
+  rm -rf ~/.vim_copy
+  echo -n "Your vim is now configurate !"
+else
+  rm -rf ~/.vim
+  mv ~/.vim_copy ~/.vim
+  echo -n "Something was wrong your config is rollback"
+fi
